a58d27b43fd109638e60b024f1d83c601896c099
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2023-10-13 12:26:40 +0500

    contact us changes

--- src/Shared/ContactUs/ContactUsForm.jsx
+++ src/Shared/ContactUs/ContactUsForm.jsx
@@ -1,17 +1,32 @@
 import Checkbox from "../Checkbox";
-import React, { memo } from "react";
-import { Link } from "react-router-dom";
 import FillBtn from "../Buttons/FillBtn";
+import { useNavigate } from "react-router-dom";
 import OutlineBtn from "../Buttons/OutlineBtn";
 import InputField from "../InputField/InputField";
+import InformationModal from "../Modal/InformationModal";
+import React, { memo, useCallback, useState } from "react";
 import { Col, Container, Form, Row, Input } from "reactstrap";
 import Images from "../../HelperMethods/Constants/ImgConstants";
 
 const ContactUsForm = () => {
+  const navigate = useNavigate();
+
+  const [showContactUsConfirmModal, setShowContactUsConfirmModal] = useState(
+    false
+  );
+
+  const handleCancelClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+  const handleSendClick = useCallback(() => {
+    setShowContactUsConfirmModal(true);
+  }, []);
+
   return (
     <Container>
       <Row className="justify-content-center align-items-center vh-100">
-        <Col md={8} className="">
+        <Col lg={7} md={12} sm={10}>
           <h1 className="text-center mb-5 f-w-bold ">Contact Us</h1>
           <Form>
             <Row md={12}>
@@ -44,37 +59,46 @@ const ContactUsForm = () => {
                   className="mb-3 py-3 px-5"
                   type="text"
                   placeholder="Phone"
-                  icon={<img src={Images.Phone_img} alt="" />}
+                  icon={<img src={Images.PHONE_ICON} alt="" />}
                 />
               </Col>
               <Col md={12}>
-                <Input
-                  className="mb-3 py-3 px-5 BorderRadius"
+                <InputField
+                  className="mb-3 py-3 px-5"
                   type="textarea"
+                  placeholder="Describe your issue"
                   name="text"
-                  id="exampleText"
                 />
               </Col>
             </Row>
-            {/* <Row className="justify-content-center my-4 gap-3">
-              <Col md={3}>
-              <FillBtn className="w-100" onClick={() => setModalShow(true)} text="Send"/>
-              </Col>
-              <Col md={3}>
-              <OutlineBtn className="w-100"  onClick={() => setModalShow(false)} text="Cancel"/>
-              
-              </Col>
-            </Row> */}
-            {/* <InfoModal
+            <FillBtn
+              className="w-100 py-3 mb-2 mt-4"
+              handleOnClick={handleSendClick}
+              text="Send"
+            />
+            <OutlineBtn
+              className="w-100 py-3"
+              handleOnClick={handleCancelClick}
+              text="Cancel"
+            />
+            <InformationModal
               size={"md"}
               TOneClassName={"mb-3 text-center"}
               TTwoClassName={"mb-3 text-center"}
-              isOpen={modalShow}
-              onClose={() => setModalShow(false)}
-              ModalText1="Your email has been sent. Our representative will reach out to you within 72 hours."
-              ModalText2="In order to serve you better, we have generated a ticket ID for your request, please check your email."
-              ButtonThree={<FillBtn className="w-50" text={"Ok"} onClick={() => setModalShow(false)}/>}
-            /> */}
+              isOpen={showContactUsConfirmModal}
+              onClose={useCallback(() => setShowContactUsConfirmModal(false))}
+              ModalTextOne="Your email has been sent. Our representative will reach out to you within 72 hours."
+              ModalTextTwo="In order to serve you better, we have generated a ticket ID for your request, please check your email."
+              ButtonThree={
+                <FillBtn
+                  className="w-50"
+                  text={"Ok"}
+                  handleOnClick={useCallback(() =>
+                    setShowContactUsConfirmModal(false)
+                  )}
+                />
+              }
+            />
           </Form>
         </Col>
       </Row>

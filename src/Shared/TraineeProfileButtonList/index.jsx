05df82e25310608efff04d9d0ca8ed9cb6514524
Syed Nayyar Mehdi <nayyarmehdi5555@gmail.com>
2023-10-19 18:53:40 +0500

    add services Provider subscription pages

--- src/Shared/TraineeProfileButtonList/index.jsx
+++ src/Shared/TraineeProfileButtonList/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Col, Row } from 'reactstrap'
-import FillBtn from '../Buttons/FillBtn'
+import React from "react";
+import { Col, Row } from "reactstrap";
+import { Link } from "react-router-dom";
+import FillBtn from "../Buttons/FillBtn";
 
 const index = () => {
   return (
-    <Row className='my-3'>
-        <Col md={12}>
-            <FillBtn className="w-100 mb-2" text="My Current Trainer"/>
-            <FillBtn className="w-100 mb-2" text="My Current Nutritionist"/>
-            <FillBtn className="w-100 mb-2" text="My Subscription History"/>
-            <FillBtn className="w-100 mb-2" text="My Progress"/>
-        </Col>
+    <Row className="my-3">
+      <Col md={12}>
+        <Link to="/trainee/ServiceProviderList">
+          <FillBtn className="w-100 mb-2" text="My Current Trainer" />
+        </Link>
+        <Link to="/trainee/ServiceProviderList">
+          <FillBtn className="w-100 mb-2" text="My Current Nutritionist" />
+        </Link>
+        <Link to="/trainee/subscriptionDetail">
+          <FillBtn className="w-100 mb-2" text="My Subscription History" />
+        </Link>
+        <FillBtn className="w-100 mb-2" text="My Progress" />
+      </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default index
+export default index;

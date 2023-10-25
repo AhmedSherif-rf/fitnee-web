59aec03f6524c83f9dfaf70f64c96515d6336c3e
Syed Nayyar Mehdi <nayyarmehdi5555@gmail.com>
2023-10-25 18:12:59 +0500

    update cerdit card

--- src/Pages/GuestPages/ServiceProviderProfile/index.jsx
+++ src/Pages/GuestPages/ServiceProviderProfile/index.jsx
@@ -4,7 +4,7 @@ import ServiceProviderProfileWrapper from "../../../Shared/ServiceProviderProfil
 const ServiceProviderProfile = () => {
   return (
     <div className="">
-      <ServiceProviderProfileWrapper />
+      <ServiceProviderProfileWrapper cardLink="/guest/serviceProvider/subscription" />
     </div>
   );
 };

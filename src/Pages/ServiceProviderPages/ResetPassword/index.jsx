cc439b63d631d4cb17fcd95d29ebe16025b6c3b3
umarfarooq32 <115708362+umarfarooq32@users.noreply.github.com>
2023-11-03 17:35:40 +0500

    change password screen and design improvements

---
+++ src/Pages/ServiceProviderPages/ResetPassword/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import ResetPasswordWrapper from "../../../Shared/ResetPassword/ResetPasswordWrapper";
+import Images from "../../../HelperMethods/Constants/ImgConstants";
+
+const ResetData = [
+  {
+    CompStyle: { backgroundImage: `url(${Images.LOGIN_BG_IMG})` },
+    text1: "",
+    text2: (
+      <div className="text-center">
+        <h1 className="fw-bold text-white">Unlock your potential</h1>
+        <span className="fs-4 text-white fst-italic">
+          & transform your body
+        </span>
+      </div>
+    ),
+    text3: <h6 className="small text-center text-white">www.fitnee.com</h6>,
+  },
+];
+
+const Reset = () => {
+  return (
+    <Container fluid className="vh-100">
+      {ResetData?.map((item) => (
+        <ResetPasswordWrapper
+          CompStyle={item.CompStyle}
+          text1={item.text1}
+          text2={item.text2}
+          text3={item.text3}
+        />
+      ))}
+    </Container>
+  );
+};
+
+export default Reset;

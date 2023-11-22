067c318b7dcfee45629fac26565bfdb57797631e
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2023-11-22 16:09:10 +0500

    set background layout color

--- src/Pages/ServiceProviderPages/Dashboard/index.jsx
+++ src/Pages/ServiceProviderPages/Dashboard/index.jsx
@@ -126,7 +126,7 @@ const Dashboard = () => {
           </div>
         </Col>
         <Col lg={9} md={8}>
-          <Card className="BorderRadius border-0">
+          <Card className="BorderRadius border-0 text-black-custom">
             <CardBody>
               <h5 className="fw-bold my-2">{t("trainer.bioText")}</h5>
               <p className="small">

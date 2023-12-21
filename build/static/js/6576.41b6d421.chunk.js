fdbd4edd0a3e3730e4446e47311f12e0384ce77d
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2023-12-21 13:04:11 +0500

    changes

---
+++ build/static/js/6576.41b6d421.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfitnee_web=self.webpackChunkfitnee_web||[]).push([[6576],{2681:function(e,n,s){s.r(n);var c=s(7823),t=s(3009),i=s(3464),r=s(5639),u=s(184);n.default=function(){return(0,u.jsx)(c.Z,{fluid:!0,children:(0,u.jsx)(t.Z,{className:"justify-content-center",children:(0,u.jsx)(i.Z,{className:"BorderRadius",children:(0,u.jsx)(r.Z,{})})})})}}}]);
+//# sourceMappingURL=6576.41b6d421.chunk.js.map

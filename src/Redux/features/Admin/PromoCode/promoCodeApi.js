7ef53059188b81c50a3b750a2e214c8b5b1d807a
NayyarNaqvi <nayyar.mehdi@argonteq.com>
2024-01-24 19:02:04 +0500

    update success toster in promo code

--- src/Redux/features/Admin/PromoCode/promoCodeApi.js
+++ src/Redux/features/Admin/PromoCode/promoCodeApi.js
@@ -20,6 +20,7 @@ export const createPromoCodeList = createAsyncThunk(
   async ({ apiEndpoint, requestData }, thunkAPI) => {
     try {
       const response = await axiosInstance.post(apiEndpoint, requestData);
+      Toaster.success("Promo code created successfully");
       return response.data;
     } catch (error) {
       Toaster.error(error?.response?.data?.error?.Message);

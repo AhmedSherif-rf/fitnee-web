a0bdff0aeb7dec373d527de620d25c3b4bda07a9
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2024-02-16 02:20:54 +0500

    remove static images

--- src/HelperMethods/Constants/ImgConstants.js
+++ src/HelperMethods/Constants/ImgConstants.js
@@ -2,132 +2,121 @@ let Images = {
   // =============================landingPageImgs===================================
   LOGO_IMG: require("../../Assets/Images/LOGO.svg").default,
   SMALL_LOGO_IMG: require("../../Assets/Images/smallLogo.svg").default,
-  GOAL_IMG: require("../../Assets/Images/homeScreen/OurGoal.png"),
-  VISION_IMG: require("../../Assets/Images/homeScreen/OurVision.png"),
-  ABOUT_IMG: require("../../Assets/Images/homeScreen/AboutFitnee.png"),
+  GOAL_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/OurGoal.png",
+  VISION_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/OurVision.png",
+  ABOUT_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/AboutFitnee.png",
   DEFAULT_USER_IMG: require("../../Assets/Images/defaultImg.svg").default,
-  FEATURE_ONE: require("../../Assets/Images/homeScreen/FeatureOneImg.png"),
-  FEATURE_TWO: require("../../Assets/Images/homeScreen/FeatureTwoImg.png"),
-  FEATURE_THREE: require("../../Assets/Images/homeScreen/FeatureThreeImg.png"),
-  FEATURE_FOUR: require("../../Assets/Images/homeScreen/FeatureFourImg.png"),
-  ARABIA_FLAG_IMG: require("../../Assets/Images/homeScreen/ArabiaFlag.svg")
-    .default,
-  INSTA_IMG: require("../../Assets/Images/homeScreen/socialMedia/Insta.svg")
-    .default,
-  TIKTOK_IMG: require("../../Assets/Images/homeScreen/socialMedia/Ticktok.svg")
-    .default,
-  AMERICAN_FLAG_IMG: require("../../Assets/Images/homeScreen/AmericanFlag.svg")
-    .default,
-  TWITTER_IMG: require("../../Assets/Images/homeScreen/socialMedia/Twitter.svg")
-    .default,
-  YOUTUBE_IMG: require("../../Assets/Images/homeScreen/socialMedia/Youtube.svg")
-    .default,
-  HERO_TEXT_BACKGROUND_IMG: require("../../Assets/Images/homeScreen/HeroTextBgImage.png"),
-  HERO_BACKGROUND_IMG:
-    require("../../Assets/Images/homeScreen/HeroBackground.svg").default,
+  FEATURE_ONE: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/FeatureOneImg.png",
+  FEATURE_TWO: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/FeatureTwoImg.png",
+  FEATURE_THREE: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/FeatureThreeImg.png",
+  FEATURE_FOUR: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/FeatureFourImg.png",
+  ARABIA_FLAG_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/ArabiaFlag.svg",
+  INSTA_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Insta.svg",
+  TIKTOK_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Ticktok.svg",
+  AMERICAN_FLAG_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/AmericanFlag.svg",
+  TWITTER_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Twitter.svg",
+  YOUTUBE_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Youtube.svg",
+  HERO_TEXT_BACKGROUND_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/HeroTextBgImage.png",
+  HERO_BACKGROUND_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/HeroBackground.svg",
   // =============================sliderImgs===================================
-  MIRROR_IMG: require("../../Assets/Images/homeScreen/sliderImgs/mirror.svg")
-    .default,
-  SLIDER3_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  SLIDER2_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  SLIDER1_IMG: require("../../Assets/tempImages/profile1.svg").default,
+  SLIDER1_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/profile1.svg",
+
   //================================loginImgs==================================
   LOGIN_BG_IMG: require("../../Assets/Images/login/loginBgImg.svg").default,
-  EMAIL_ICON: require("../../Assets/Images/InputField/emailLogo.svg").default,
-  PHONE_ICON: require("../../Assets/Images/InputField/phoneLogo.svg").default,
-  PERSON_ICON: require("../../Assets/Images/InputField/personLogo.svg").default,
+  EMAIL_ICON: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/emailLogo.svg",
+  PHONE_ICON: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/phoneLogo.svg",
+  PERSON_ICON: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/personLogo.svg",
   //==============================registerAsImgs===============================
-  REGISTER_AS_BG:
-    require("../../Assets/Images/registerAsScreen/RegisterAsBackground.svg")
-      .default,
+  REGISTER_AS_BG: require("../../Assets/Images/registerAsScreen/RegisterAsBackground.svg")
+    .default,
   //==============================GuestServiceProviderListImgs=================
-  PROFILE1_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  PROFILE2_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  PROFILE3_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  PROFILE4_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  PROFILE5_IMG: require("../../Assets/tempImages/profile1.svg").default,
-  ARROW_RIGHT_IMG: require("../../Assets/Images/ConstImgs/RightArrow.svg")
+  PROFILE2_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/profile1.svg",
+  PROFILE3_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/profile1.svg",
+  PROFILE4_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/profile1.svg",
+  PROFILE5_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/profile1.svg",
+  ARROW_RIGHT_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/RightArrow.svg",
+  SEE_MORE_BG_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/SeeMoreBg.svg",
+  SHORTLOGO_IMG: require("../../Assets/Images/serviceProviderListScreen/DumbleLogo.svg")
     .default,
-  SEE_MORE_BG_IMG: require("../../Assets/Images/ConstImgs/SeeMoreBg.svg")
-    .default,
-  SHORTLOGO_IMG:
-    require("../../Assets/Images/serviceProviderListScreen/DumbleLogo.svg")
-      .default,
   //==============================GuestServiceProviderProviderImgs=============
-  COMMENT_IMG:
-    require("../../Assets/Images/serviceProviderListScreen/TrainerImg.svg")
-      .default,
-  DOCUMENT_IMG:
-    require("../../Assets/Images/serviceProviderListScreen/DocumentImg.svg")
-      .default,
+  COMMENT_IMG: require("../../Assets/Images/serviceProviderListScreen/TrainerImg.svg")
+    .default,
+  DOCUMENT_IMG: require("../../Assets/Images/serviceProviderListScreen/DocumentImg.svg")
+    .default,
   //==============================SubscriptionImgs=============================
   TRAINER_ONE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/trainerOneMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/trainerOneMonth.svg",
   TRAINER_TWO_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/trainerTwoMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/trainerTwoMonth.svg",
   TRAINER_THREE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/trainerThreeMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/trainerThreeMonth.svg",
 
   NUTRITIONIST_ONE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/nutritionistOneMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/nutritionistOneMonth.svg",
   NUTRITIONIST_TWO_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/nutritionistTwoMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/nutritionistTwoMonth.svg",
   NUTRITIONIST_THREE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/nutritionistThreeMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/nutritionistThreeMonth.svg",
 
   BOTH_T_AND_N_ONE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/bothOneMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/bothOneMonth.svg",
   BOTH_T_AND_N_TWO_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/bothTwoMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/bothTwoMonth.svg",
   BOTH_T_AND_N_THREE_MONTH_IMG:
-    require("../../Assets/Images/ConstImgs/subscription/bothThreeMonth.svg")
-      .default,
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/bothThreeMonth.svg",
 
-  VISA_ICON: require("../../Assets/Images/ConstImgs/visaIcon.png"),
-  MADA_ICON: require("../../Assets/Images/ConstImgs/madaIcon.png"),
-  STC_PAY_ICON: require("../../Assets/Images/ConstImgs/stcPayIcon.png"),
-  APPLE_PAY_ICON: require("../../Assets/Images/ConstImgs/applePayIcon.png"),
+  VISA_ICON:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/visaIcon.png",
+  MADA_ICON:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/madaIcon.png",
+  STC_PAY_ICON:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/stcPayIcon.png",
+  APPLE_PAY_ICON:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/applePayIcon.png",
   //==============================ConstImgs====================================
-  ARROW_DOWN_IMG: require("../../Assets/Images/ConstImgs/ArrowDown.svg")
-    .default,
-  CAMERA_IMG: require("../../Assets/Images/ConstImgs/Frame.svg").default,
-  ARROW_UP_IMG: require("../../Assets/Images/ConstImgs/ArrowUp.svg").default,
-  CREDIT_CARD_IMG: require("../../Assets/Images/ConstImgs/CreditCardImg.svg")
-    .default,
-  PAYMENT_METHOD_IMG: require("../../Assets/Images/ConstImgs/PaymentMethod.png")
-    .default,
-  CARD_ICON_IMG: require("../../Assets/Images/ConstImgs/CardIcon.svg").default,
-  GREEN_CIRCLE_IMG: require("../../Assets/Images/ConstImgs/greenCircle.png")
-    .default,
-  YELLOW_CIRCLE_IMG: require("../../Assets/Images/ConstImgs/yellowCircle.png")
-    .default,
-  UPLOAD_BG_IMG: require("../../Assets/Images/ConstImgs/uploadImg.svg").default,
-  UPLOAD_ICON: require("../../Assets/Images/ConstImgs/uploadImg.svg").default,
-  PASSWORD_ICON_IMG: require("../../Assets/Images/ConstImgs/passwordIcon.svg")
-    .default,
-  USER_DUMMY_IMG: require("../../Assets/Images/ConstImgs/userDummyImg.png"),
-  NO_DATA_FOUND_IMG: require("../../Assets/Images/ConstImgs/noDataFound.png"),
+  ARROW_DOWN_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/ArrowDown.svg",
+  CAMERA_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Frame.svg",
+  ARROW_UP_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/ArrowUp.svg",
+  CREDIT_CARD_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/CreditCardImg.svg",
+  PAYMENT_METHOD_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/PaymentMethod.png",
+  CARD_ICON_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/CardIcon.svg",
+  GREEN_CIRCLE_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/greenCircle.png",
+  YELLOW_CIRCLE_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/yellowCircle.png",
+  UPLOAD_BG_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/uploadImg.svg",
+  UPLOAD_ICON:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/uploadImg.svg",
+  PASSWORD_ICON_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/passwordIcon.svg",
+  USER_DUMMY_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/userDummyImg.png",
+  NO_DATA_FOUND_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/noDataFound.png",
   //==============================Trainee Images====================================
-  TRAINEE_GRAPH_IMG: require("../../Assets/Images/TraineeGraph.svg").default,
-  COUNTER_BG_IMG: require("../../Assets/Images/homeScreen/CountBgImage.jpg")
-    .default,
-  FEATURE_BG_IMG: require("../../Assets/Images/homeScreen/featureBg.svg")
-    .default,
+  FEATURE_BG_IMG: "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/featureBg.svg",
   //==============================appStore and Google btn Images====================================
   APP_STORE_IMG: require("../../Assets/Images/appStore.svg").default,
-  CVV_IMG: require("../../Assets/Images/ConstImgs/Hint.svg").default,
+  CVV_IMG:
+    "https://fitme-dev-bucket.s3.eu-north-1.amazonaws.com/media/webStatic/Hint.svg",
   GOOGLE_PLAY_IMG: require("../../Assets/Images/googlePlay.svg").default,
 
-   //==============================QR Code Img====================================
-   QR_CODE_IMG: require("../../Assets/Images/QRcodeImg/QRImage.svg").default,
+  //==============================QR Code Img====================================
+  QR_CODE_IMG: require("../../Assets/Images/QRcodeImg/QRImage.svg").default,
 };
 
 export default Images;

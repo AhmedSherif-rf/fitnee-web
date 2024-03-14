5aa6867030a60f824d14817ea6b986f3d805d401
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2024-03-14 13:57:21 +0500

    change in notificaiton body

--- src/App.js
+++ src/App.js
@@ -54,16 +54,16 @@ function App() {
 
   onMessageListener()
     .then((payload) => {
-      console.log("notification payload", payload)
-      // setNotification({
-      //   title: payload.notification.title,
-      //   body: payload.notification.body,
-      // });
-      // fetchUserNotifications();
-      // setShowNotification(true);
-      // setTimeout(() => {
-      //   setShowNotification(false);
-      // }, 5000);
+      const body = JSON.parse(payload.notification.body);
+      setNotification({
+        title: payload.notification.title,
+        body: body.msg,
+      });
+      fetchUserNotifications();
+      setShowNotification(true);
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 5000);
     })
     .catch((err) => console.log("failed: ", err));
 

fa56ba84a53634587a7c7cd090ed290e7a6c1b5b
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2024-03-20 21:26:32 +0500

    build created

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@
 export const CURRENCY = "SAR";
 //===============AdminCredentials==================
 export const ADMIN_EMAIL = "admin@admin.com";
-//======================paginationParams============
+//======================paginationParams===========
 export const PAGE_RANGE = 2;
 export const PER_PAGE_COUNT = 8;
-//======================languages===================
+//======================languages==================
 export const ARABIC_LANGUAGE = "ar";
 export const ENGLISH_LANGUAGE = "en";
 export const DEFAULT_LANGUAGE = "en";

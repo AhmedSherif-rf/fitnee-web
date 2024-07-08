bae091a058f213980d0a2e24118284bc6b18f3a8
waleed khalid <waleed.khalid@argonteq.com>
2024-07-09 00:04:52 +0500

    change in button

--- src/Pages/Layout/GeneralLayout/index.jsx
+++ src/Pages/Layout/GeneralLayout/index.jsx
@@ -1,12 +1,14 @@
 import { Container } from "reactstrap";
 import React, { useEffect } from "react";
 import styles from "./style.module.scss";
+import { useSelector } from "react-redux";
 import TopBar from "../../../Shared/TopBar";
 import { FaWhatsapp } from "react-icons/fa6";
 import { useLocation, Link } from "react-router-dom";
 
 const GeneralLayout = (props) => {
   const { pathname } = useLocation();
+  const { user } = useSelector((state) => state.user);
 
   useEffect(() => {
     window.scrollTo({
@@ -24,17 +26,19 @@ const GeneralLayout = (props) => {
           isPrivate={props.isPrivate}
           isAuth={props.isAuth}
         />
-        <button
-          className={`rounded-circle position-fixed ${styles.whatsappBtnOverlay} bottom-0 m-3  z-3  border-0 shadow-md p-2`}
-        >
-          <Link
-            className="text-decoration-none"
-            to="https://wa.me/+966549836605"
-            target="_blank"
+        {user === null && (
+          <button
+            className={`rounded-circle position-fixed ${styles.whatsappBtnOverlay} bottom-0 m-3  z-3  border-0 shadow-md p-2`}
           >
-            <FaWhatsapp className="text-white" size="30px" />
-          </Link>
-        </button>
+            <Link
+              className="text-decoration-none"
+              to="https://wa.me/+966549836605"
+              target="_blank"
+            >
+              <FaWhatsapp className="text-white" size="30px" />
+            </Link>
+          </button>
+        )}
         <Container
           fluid
           className={`p-0 ${
